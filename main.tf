dea2e95d393d5ec912f45213fde85fd5141b4819
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-04-22 14:27:30 +0530

    added the script

--- main.tf
+++ main.tf
@@ -51,15 +51,7 @@ resource "google_compute_instance" "demo" {
   }
 
   # We can install any tools we need for the demo in the startup script
-  metadata_startup_script = <<EOT
-  sleep 15m
-  sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-  sudo mkdir -p /data
-  sudo mount -o discard,defaults /dev/sdb /data
-  sudo chmod a+w /data
-  sudo cp /etc/fstab /etc/fstab.backup
-  echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /data ext4 discard,defaults,noatime,nofail 0 2 | sudo tee -a /etc/fstab
-EOT
+  metadata_startup_script = file("${path.root}/start_script.sh")
 
   depends_on = [google_compute_address.external-static-ip]
 
