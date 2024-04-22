aff002f8b97401c116a02ec1d5d270a04914e1e8
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-04-22 13:01:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,12 +52,13 @@ resource "google_compute_instance" "demo" {
 
   # We can install any tools we need for the demo in the startup script
   metadata_startup_script = <<EOT
-    sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb 
-    sudo mkdir -p /data-mount 
-    sudo mount -o discard,defaults /dev/sdb /data-mount 
-    sudo chmod a+w /data-mount 
-    sudo cp /etc/fstab /etc/fstab.backup 
-    echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /data-mount ext4 discard,defaults,noatime,nofail 0 2 | sudo tee -a /etc/fstab 
+  sleep 15m
+  sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
+  sudo mkdir -p /data-mount
+  sudo mount -o discard,defaults /dev/sdb /data-mount
+  sudo chmod a+w /data-mount
+  sudo cp /etc/fstab /etc/fstab.backup
+  echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /data-mount ext4 discard,defaults,noatime,nofail 0 2 | sudo tee -a /etc/fstab
 EOT
 
   depends_on = [google_compute_address.external-static-ip]
